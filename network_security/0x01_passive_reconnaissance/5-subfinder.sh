963f43327824176f8b76baec397205bc102a8145
Fidan <aslanovafidan299@gmail.com>
2025-08-16 19:20:16 -0400

    My task

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-domain=$1
-subfinder -d "$domain" -silent | while read host; do
-    ip=$(dig +short "$host" | head -n1)
-    if [ -n "$ip" ]; then
-        echo "$host,$ip"
-    fi
-done > "$domain.txt"
+subfinder -d "$1" -silent | while read host; do ip=$(dig +short "$host" | head -n1); [ -n "$ip" ] && echo "$host,$ip"; done > "$1.txt"
