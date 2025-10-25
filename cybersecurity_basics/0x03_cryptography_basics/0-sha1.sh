f23575c79977a7bfda0daf35d2b6519f006bda43
Fidan <aslanovafidan299@gmail.com>
2025-10-25 11:17:12 +0000

    my task

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Script that hashes a given password using SHA-1 and stores it in 0_hash.txt
-
-# $1 = first argument (the password)
 echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
-
